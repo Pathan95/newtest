d9b6099cf131a41d74184c76685e77ea93e9deb4
pathan <devilskingali@gmail.com>
2023-07-22 04:50:03 +0000

    task.java

---
+++ task.java
@@ -0,0 +1,10 @@
+jhvbbvjhbxl baygalskckljadsyvuglxkc kjzfhv ak k;o aiubclahbn jhzsb
+c dkck hgsvc hzsc naknclkdfiuhsnvksnclnzlkvbayugsdnjhsgyuwe;oAKNFLSBVYA
+jhvsbvhbzxhjb.mmvkdfvlhsdbl zxvjkhzbhhblhbsb jknkbjgiljkydrlkjpih;jkbkjnc,hjvdstgfandhan
+.nc sghkbcj jkb ,vjarjnk sjbjhvbsl vkjsfghawjnzsbvlhasvs,mc;lfohcsdjkhsjvsnva
+kvbjksbvsapfnaNMCIOSNC.,M C;LSKN
+kksvlkbsvlkzj,.m skjnasnn;nsmd
+skkv lksbvjasvm s;lkvn
+skv sblab
+sdn abdljshb
+
